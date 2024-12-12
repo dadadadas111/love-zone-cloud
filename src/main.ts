35d0924b7e15ba6b154ac8223c9b4a670584db06
dadadadas111 <long6athcskl@gmail.com>
2024-12-12 10:34:39 +0700

    fix: main

--- src/main.ts
+++ src/main.ts
@@ -4,12 +4,6 @@ import { ValidationPipe } from '@nestjs/common';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  app.enableCors({
-    origin: 'https://fumi.fyi',
-    methods: ['GET', 'POST', 'PUT', 'DELETE'],
-    credentials: true,
-  });
-  app.setGlobalPrefix('api');
   app.useGlobalPipes(new ValidationPipe());
   await app.listen(process.env.PORT || 3000);
 }
