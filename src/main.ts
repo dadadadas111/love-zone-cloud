ce89aacbd6dcc1640d8a96f788c5c3b8a824b95b
dadadadas111 <long6athcskl@gmail.com>
2024-12-20 14:23:42 +0700

    fix: allow cors

--- src/main.ts
+++ src/main.ts
@@ -5,6 +5,13 @@ import { Transport } from '@nestjs/microservices';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  app.enableCors(
+    {
+      origin: '*',
+      methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
+      allowedHeaders: 'Content-Type, Accept',
+    },
+  );
   app.useGlobalPipes(new ValidationPipe());
   try {
     const microservice = app.connectMicroservice({
